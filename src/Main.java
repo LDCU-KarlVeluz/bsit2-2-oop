9644606b86761536569f0accfa187955ea37aada
Ritchie Karl Veluz <rkveluz68722@liceo.edu.ph>
2025-08-18 11:48:05 +0800

    activity 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        GradeCalculator gc = new GradeCalculator();
+
+        String studentName = "John Smith";
+        double average = gc.calculateAverage(85.5, 92.0, 78.5, 90.0);
+        String letterGrade = gc.getLetterGrade(average);
+
+        gc.displayResult(studentName, average);
+        System.out.println();
+        gc.displayResult(studentName, average, letterGrade);
     }
 }
